20d66ab345d8ad8df59bbb4a123d9e1c555d93c9
Adebola Adeniran <adebola@Adebolas-MacBook-Pro.local>
2023-12-21 18:21:20 +0000

    manually merge main into dev

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,10 @@
-import { eUSD_ADDRESS } from "../constants/addresses";
+import {
+  eUSD_ADDRESS,
+  USDT_ADDRESS,
+  USDC_ADDRESS,
+  BTC_ADDRESS,
+  ETH_ADDRESS,
+} from "../constants/addresses";
 import { useMemo, useState } from "react";
 import {
   Box,
@@ -12,9 +18,14 @@ import {
 import { IWeb3Context, useWeb3Context } from "../context/Web3Context";
 import { MdCheck, MdError } from "react-icons/md";
 import useDrip from "../hooks/useDrip";
+import etherlinkIcon from "../public/etherlink.svg";
+import "primeicons/primeicons.css";
+import { BiLogOut } from "react-icons/bi";
 import Navbar from "../components/landing/Navbar";
 import FaucetTable from "../components/landing/FaucetTable";
 import Footer from "../components/landing/Footer";
+
+import { Button as PrimeReactButton } from "primereact/button";
 
 const chainId = 128123;
 
@@ -42,9 +53,8 @@ export default function Home() {
 
   return (
     <div className="dark:bg-etherlink-bg min-h-screen">
+      {/* START */}
       <div className="container max-w-7xl mx-auto">
-        {/* START */}
-
         <Navbar
           walletAddress={address}
           isConnected={isAuthenticated}
@@ -52,52 +62,94 @@ export default function Home() {
           disconnectWallet={disconnect}
         />
         <FaucetTable loadingDrip={loading} drip={drip} />
-
         <div className="mt-5">
           <Footer />
         </div>
-        {/* END */}
+      </div>
+      {/* END */}
 
-        <Button onClick={() => drip(eUSD_ADDRESS)}>Drip</Button>
-        {isAuthenticated &&
-          (correctNetwork ? (
-            <VStack ml={4} mt={4} spacing={4} alignItems="flex-start">
-              <Box
-                onSubmit={handleSubmit}
-                as="form"
-                display="flex"
-                flexDirection="column"
-                gap={4}
-              >
-                <Input
-                  type="text"
-                  placeholder="Enter token address..."
-                  variant="flushed"
-                  colorScheme="blue"
-                  name="address"
-                  value={eUSD_ADDRESS}
-                  onChange={(e) => setNewMessage(e.target.value)}
-                />
+      <div className="bg-white">
+        <HStack
+          width="full"
+          as="header"
+          height="80px"
+          px={4}
+          alignItems="center"
+          bg="gray.100"
+        >
+          <HStack as="nav" width="full" justifyContent="space-between">
+            <HStack>
+              {!isAuthenticated ? (
+                // <PrimeReactButton icon={(options) =>
+                //   <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" {...options.iconProps}>
+                //       <g id="chevron-down">
+                //           <path d="M12,15.25a.74.74,0,0,1-.53-.22l-5-5A.75.75,0,0,1,7.53,9L12,13.44,16.47,9A.75.75,0,0,1,17.53,10l-5,5A.74.74,0,0,1,12,15.25Z"/>
+                //       </g>
+                //   </svg>}
+                //   onClick={connectWallet}
+                //   label="Connect wallet" />
                 <Button
-                  type="submit"
+                  onClick={connectWallet}
                   variant="solid"
                   bg="green.400"
                   colorScheme="green"
+                  gap={2}
+                  color="white"
+                >
+                  <i className="pi pi-wallet"></i>
+                  Connect wallet
+                </Button>
+              ) : (
+                <Button
+                  onClick={disconnect}
+                  variant="solid"
+                  bg="red.400"
+                  colorScheme="red"
                   color="white"
                   gap={2}
-                  isLoading={loading}
                 >
-                  <Icon as={MdCheck} />
-                  Submit
+                  <Icon as={BiLogOut} />
+                  Disconnect
                 </Button>
-              </Box>
-            </VStack>
-          ) : (
-            <HStack spacing={2} ml={4} mt={4}>
-              <Icon as={MdError} color="red.400" />
-              <Text color="red.400">Please switch to Etherlink Testnet</Text>
+              )}
             </HStack>
-          ))}
+          </HStack>
+        </HStack>
+        <PrimeReactButton
+          onClick={() => drip(eUSD_ADDRESS)}
+          label="Get eUSD"
+          raised
+          rounded
+          badge="10"
+        ></PrimeReactButton>
+        <PrimeReactButton
+          onClick={() => drip(USDT_ADDRESS)}
+          label="Get USDT"
+          raised
+          rounded
+          badge="10"
+        ></PrimeReactButton>
+        <PrimeReactButton
+          onClick={() => drip(USDC_ADDRESS)}
+          label="Get USDC"
+          raised
+          rounded
+          badge="10"
+        ></PrimeReactButton>
+        <PrimeReactButton
+          onClick={() => drip(BTC_ADDRESS)}
+          label="Get BTC"
+          raised
+          rounded
+          badge="0.001"
+        ></PrimeReactButton>
+        <PrimeReactButton
+          onClick={() => drip(ETH_ADDRESS)}
+          label="Get ETH"
+          raised
+          rounded
+          badge="0.01"
+        ></PrimeReactButton>
       </div>
     </div>
   );
